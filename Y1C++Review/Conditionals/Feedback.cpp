a605071e015d73b62d4786c7b67a03b45b9128cc
Conner-Stack <connerstack@gmail.com>
2017-08-04 16:22:38 -0500

    conditionals review portion
    finished the coding for the conditionals portion on the review. Still
    must proofread and revise my comments for better clarity

--- Y1C++Review/Conditionals/Feedback.cpp
+++ Y1C++Review/Conditionals/Feedback.cpp
@@ -40,74 +40,81 @@ int main()
 
 
 	//2. Write an if statement that assigns 100 to the value of x when y is equal to zero
-	int x;
+	int x= 0;
 	int y;
+	cout << "Insert a number for 'y'" << endl;
+	cin>>y;
 	if (y == 0)
 		x = 100;
+	cout << x << endl;
+		//3. Write a program that accepts an integer that represents the month of the year
+		//It should then display the number of days in that month. If a number that doesn’t correspond 
+		//to a month is entered then the program should display an error message. You must use a switch statement
 
-	//3. Write a program that accepts an integer that represents the month of the year
-	//It should then display the number of days in that month. If a number that doesn’t correspond 
-	//to a month is entered then the program should display an error message. You must use a switch statement
-
-	int month;
-	cin >> month;
-	switch (month)
-	{
-	case 1:
-		cout << 31 << endl;
-		break;
-	case 2:
-		cout << 28 / 29 << endl;
-		break;
-	case 3:
-		cout << 31 << endl;
-		break;
-	case 4:
-		cout << 30 << endl;
-		break;
-	case 5:
-		cout << 31 << endl;
-		break;
-	case 6:
-		cout << 30 << endl;
-		break;
-	case 7:
-		cout << 31 << endl;
-		break;
-	case 8:
-		cout << 30 << endl;
-		break;
-	case 9:
-		cout << 31 << endl;
-		break;
-	case 10:
-		cout << 30 << endl;
-		break;
-	case 11:
-		cout << 31 << endl;
-		break;
-	case 12:
-		cout << 30 << endl;
-		break;
-	default:
-		cout << "error, input does not correspond to a month" << endl;
-	}
-	//4. Redo problem 3 using if statements
-	month = 0;
-	cin >> month;
-	if (month <= 12) {
-		if (month % 2 == 0 && month != 2)
-			cout << 30 << endl;
-		else if (month % 2 == 1)
+		int month;
+		cin >> month;
+		switch (month)
+		{
+		case 1:
 			cout << 31 << endl;
-		else if (month == 2)
-			cout << 28 << '/' << 29 << endl;
+			break;
+		case 2:
+			cout<<28<<" or "<<29<<endl;
+			break;
+		case 3:
+			cout << 31 << endl;
+			break;
+		case 4:
+			cout << 30 << endl;
+			break;
+		case 5:
+			cout << 31 << endl;
+			break;
+		case 6:
+			cout << 30 << endl;
+			break;
+		case 7:
+			cout << 31 << endl;
+			break;
+		case 8:
+			cout << 30 << endl;
+			break;
+		case 9:
+			cout << 31 << endl;
+			break;
+		case 10:
+			cout << 30 << endl;
+			break;
+		case 11:
+			cout << 31 << endl;
+			break;
+		case 12:
+			cout << 30 << endl;
+			break;
+		default:
+			cout << "error, input does not correspond to a month" << endl;
+		}
+		//4. Redo problem 3 using if statements
+		month = 0;
+		cout << "input the number representation of a month of the year" << endl;
+		cin >> month;
+		if (month <= 12) {
+			if (month % 2 == 0 && month != 2)
+				cout << 30 << endl;
+			else if (month % 2 == 1)
+				cout << 31 << endl;
+			else if (month == 2)
+				cout << 28 << " or " << 29 << endl;
+		}
+		else
+			cout << "error, input does not correspond to a month" << endl;
+
+		//5. Convert problem 2 into a ternary operator
+		cout << "input a number for 'y'" << endl;
+		cin >> y;
+	x = (y == 0) ? 100 : 0;
+		cout << x << endl;
+		system("pause");
+		return 0;
 	}
-	else
-		cout << "error, input does not correspond to a month" << endl;
 
-	//5. Convert problem 2 into a ternary operator
-
-	system("pause");
-	return 0;
-}
