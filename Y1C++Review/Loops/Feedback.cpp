f3157fb5dc3da50795cac6e36fdd084486485a5e
Conner-Stack <connerstack@gmail.com>
2017-08-07 11:19:32 -0500

    final fix
    final fix

--- Y1C++Review/Loops/Feedback.cpp
+++ Y1C++Review/Loops/Feedback.cpp
@@ -98,14 +98,14 @@ int main()
 	//Note, you will need to use the modulus operator (%)
 	for (int i = 0; i <= 100; i++)
 	{
-		if (iter % 3 == 0 && iter % 5 != 0)
+		if (i % 3 == 0 && i % 5 != 0)
 			cout << "fizz" << endl;
-		else if (iter % 5 == 0 && iter % 3 != 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 			cout << "buzz" << endl;
-		else if (iter % 3 == 0 && iter % 5 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 			cout << "fizzbuzz" << endl;
 		else
-			cout << iter << endl;
+			cout << i << endl;
 		iter++;
 	}
 
